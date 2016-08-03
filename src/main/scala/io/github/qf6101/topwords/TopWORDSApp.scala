ac9bc4d94d3736c9d7e89bddc5039a5e43135758
hzqianfeng <hzqianfeng@corp.netease.com>
2016-08-03 11:47:35 +0800

    dump

--- src/main/scala/io/github/qf6101/topwords/TopWORDSApp.scala
+++ src/main/scala/io/github/qf6101/topwords/TopWORDSApp.scala
@@ -9,7 +9,7 @@ import org.apache.spark.storage.StorageLevel
   * Created by qfeng on 16-7-18.
   */
 object TopWORDSApp extends Serializable {
-  @transient private[this] val LOGGER = Logger.getLogger(TopWORDSApp.getClass.toString)
+  @transient private[this] val LOGGER = Logger.getLogger(this.getClass.toString)
 
   def main(args: Array[String]) {
     // setup spark session
@@ -17,22 +17,24 @@ object TopWORDSApp extends Serializable {
     import spark.implicits._
     try {
       TopWORDSParser.parse(args).foreach { args =>
+        spark.read.format(args.inputFormat).load(args.inputLoc).map(_.toString()).write.text(args.outputLoc +
+          "newscorpus")
+
         // remove output location files if exist
-        val files = FileSystem.get(spark.sparkContext.hadoopConfiguration)
-        if (files.exists(new Path(args.outputLoc))) files.delete(new Path(args.outputLoc), true)
-        // read input corpus
-        val corpus = spark.read.format(args.inputFormat).load(args.inputLoc).map(_.toString())
-          .repartition(args.numIterations).persist(StorageLevel.MEMORY_AND_DISK_SER_2)
-        // run TopWORDS with the parsed arguments
-        new TopWORDS(
-          tauL = args.tauL,
-          tauF = args.tauF,
-          textLenThld = args.textLenThld,
-          useProbThld = args.useProbThld,
-          numIterations = args.numIterations,
-          convergeTol = args.convergeTol,
-          wordBoundaryThld = args.wordBoundaryThld)
-          .run(corpus, args.outputLoc + "/dictionary", args.outputLoc + "/segmented_texts")
+//        val files = FileSystem.get(spark.sparkContext.hadoopConfiguration)
+//        if (files.exists(new Path(args.outputLoc))) files.delete(new Path(args.outputLoc), true)
+//        // read input corpus
+//        val corpus = spark.read.format(args.inputFormat).load(args.inputLoc).map(_.toString()).repartition(args.numIterations)
+//        // run TopWORDS with the parsed arguments
+//        new TopWORDS(
+//          tauL = args.tauL,
+//          tauF = args.tauF,
+//          textLenThld = args.textLenThld,
+//          useProbThld = args.useProbThld,
+//          numIterations = args.numIterations,
+//          convergeTol = args.convergeTol,
+//          wordBoundaryThld = args.wordBoundaryThld)
+//          .run(corpus, args.outputLoc + "/dictionary", args.outputLoc + "/segmented_texts")
       }
       //exit normally
       LOGGER.info("Running TopWORDS successfully!")
