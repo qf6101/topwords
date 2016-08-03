31f865f141f735d4c4476693b453ee705e4cd2a8
qfeng <hzqianfeng@corp.netease.com>
2016-08-03 15:03:01 +0800

    set script to test environment

--- deploy/sbin/topwords_yarn.sh
+++ deploy/sbin/topwords_yarn.sh
@@ -6,14 +6,14 @@ cd "$( cd "$( dirname "$0"  )" && pwd  )"
 ##### The Parameters You Need to Predefine Start #####
 
 # set the environment variables
-HADOOP_HOME="/path/to/hadoop/home"  #Hadoop home
-SPARK_HOME="/path/to/spark/home"  #Spark home
+HADOOP_HOME="/home/nlp/hadoop-xs"
+SPARK_HOME="/home/nlp/spark-xs.2.0.0"  #Spark home
 topwords_jar="../release/topwords-1.0.jar"  #topwords jar file
 
 # set the arguments
-inputLoc="/path/to/corpus/in/hdfs/*"  #location of input corpus in HDFS
-inputFormat="text"  #format of input files
-outputLoc="/path/to/output/in/hdfs" #location of output dictionary and segmented corpus in HDFS
+inputLoc="qfeng/topwords/newscorpus"  #location of input corpus in HDFS
+inputFormat="text"  #format of input files (parquet/json/text)
+outputLoc="qfeng/topwords/newscorpus_output" #location of output dictionary and segmented corpus in HDFS
 tauL="10"  #threshold of word length
 tauF="5"  #threshold of word frequency
 numIterations="10"  #number of iterations
@@ -21,11 +21,11 @@ convergeTol="1E-3"  #convergence tolerance
 textLenThld="2000"  #preprocessing threshold of text length
 useProbThld="1E-8"  #prune threshold of word use probability
 wordBoundaryThld="0.0"  #segment threshold of word boundary score (use segment tree if set to <= 0)
-numPartitions="100"  #number of partitions
-executor_memory="2G"  #memory allocation for each executor
-num_executors="10"  #number of executors allocated
-executor_cores="2"  #number of cores allocated for each executor
-queue="queue_name"  #yarn queue
+numPartitions="5000"  #number of partitions
+executor_memory="5G"  #memory allocation for each executor
+num_executors="30"  #number of executors allocated
+executor_cores="3"  #number of cores allocated for each executor
+queue="nlp"  #yarn queue
 
 ##### The Parameters You Need to Predefine End #####
 
@@ -35,7 +35,8 @@ queue="queue_name"  #yarn queue
 function_exec(){
 ${SPARK_HOME}/bin/spark-submit \
 --class io.github.qf6101.topwords.TopWORDSApp \
---master yarn-cluster \
+--master yarn \
+--deploy-mode cluster \
 --name topwords \
 --executor-memory $executor_memory \
 --num-executors $num_executors \
@@ -62,8 +63,7 @@ sleep 60s
 app_id=`grep -Eo "application_[0-9]+_[0-9]+" ${output} | head -n1`
 logfile=${output}.log
 ${HADOOP_HOME}/bin/yarn logs -applicationId ${app_id} > ${logfile}
-
-#check the result state
+#check execution result
 if grep -i ERROR ${logfile}
 then
         rm $output
