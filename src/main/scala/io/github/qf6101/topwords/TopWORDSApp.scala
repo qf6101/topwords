467c4550494dd6679c231a6853078d93f7678fc6
hzqianfeng <hzqianfeng@corp.netease.com>
2016-08-03 11:55:32 +0800

    dump

--- src/main/scala/io/github/qf6101/topwords/TopWORDSApp.scala
+++ src/main/scala/io/github/qf6101/topwords/TopWORDSApp.scala
@@ -17,8 +17,7 @@ object TopWORDSApp extends Serializable {
     import spark.implicits._
     try {
       TopWORDSParser.parse(args).foreach { args =>
-        spark.read.format(args.inputFormat).load(args.inputLoc).map(_.toString()).write.text(args.outputLoc +
-          "newscorpus2")
+        spark.read.format(args.inputFormat).load(args.inputLoc).map(_.getAs[String](0)).write.text(args.outputLoc)
 
         // remove output location files if exist
 //        val files = FileSystem.get(spark.sparkContext.hadoopConfiguration)
