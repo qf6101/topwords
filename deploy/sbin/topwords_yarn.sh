445817afa4412277738993c370cdc3115510339f
qf6101 <m13588246101@163.com>
2016-08-05 14:48:55 +0800

    Update topwords_yarn.sh

--- deploy/sbin/topwords_yarn.sh
+++ deploy/sbin/topwords_yarn.sh
@@ -6,26 +6,26 @@ cd "$( cd "$( dirname "$0"  )" && pwd  )"
 ##### The Parameters You Need to Predefine Start #####
 
 # set the environment variables
-HADOOP_HOME="/home/nlp/hadoop-xs"
-SPARK_HOME="/home/nlp/spark-xs.2.0.0"  #Spark home
+HADOOP_HOME="/path/to/hadoop"  #Hadoop home
+SPARK_HOME="/path/to/spark"  #Spark home
 topwords_jar="../release/topwords-1.0.jar"  #topwords jar file
 
 # set the arguments
-inputLoc="qfeng/topwords/newscorpus"  #location of input corpus in HDFS
-inputFormat="text"  #format of input files (parquet/json/text)
-outputLoc="qfeng/topwords/newscorpus_output" #location of output dictionary and segmented corpus in HDFS
-tauL="10"  #threshold of word length
-tauF="5"  #threshold of word frequency
-numIterations="10"  #number of iterations
+inputLoc="/path/to/corpus/in/hdfs/*"  #location of input corpus in HDFS
+inputFormat="text"  #format of input files (parquet/json/text) 
+outputLoc="/path/to/output/in/hdfs" #location of output dictionary and segmented corpus in HDFS
+tauL="6"  #threshold of word length
+tauF="100"  #threshold of word frequency
+numIterations="5"  #number of iterations
 convergeTol="1E-3"  #convergence tolerance
 textLenThld="2000"  #preprocessing threshold of text length
 useProbThld="1E-8"  #prune threshold of word use probability
 wordBoundaryThld="0.0"  #segment threshold of word boundary score (use segment tree if set to <= 0)
 numPartitions="5000"  #number of partitions
 executor_memory="5G"  #memory allocation for each executor
-num_executors="30"  #number of executors allocated
-executor_cores="3"  #number of cores allocated for each executor
-queue="nlp"  #yarn queue
+num_executors="40"  #number of executors allocated
+executor_cores="1"  #number of cores allocated for each executor
+queue="queue_name"  #yarn queue
 
 ##### The Parameters You Need to Predefine End #####
 
